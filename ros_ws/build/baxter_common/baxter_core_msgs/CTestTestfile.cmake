f6e2bce4a1cec0cb516252676f6da8d7973f0165
Catherine Zhao <caz2114@barnard.edu>
2018-04-18 17:39:55 -0400

    removre ros_ws

--- ros_ws/build/baxter_common/baxter_core_msgs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/parallels/ros_ws/src/baxter_common/baxter_core_msgs
-# Build directory: /home/parallels/ros_ws/build/baxter_common/baxter_core_msgs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros_ws/build/baxter_examples/setup_custom_pythonpath.sh
+++
@@ -1,4 +0,0 @@
-# generated from dynamic_reconfigure/cmake/setup_custom_pythonpath.sh.in
-
-PYTHONPATH=/home/parallels/ros_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH
-exec "$@"
